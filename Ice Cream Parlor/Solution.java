a24cc1175905ccdd4392e5dc73054d20182cf4b7
AshirwadVyawahare <47682118+AshirwadVyawahare@users.noreply.github.com>
2020-04-26 19:11:28 +0530

    Create Solution.java

---
+++ Ice Cream Parlor/Solution.java
@@ -0,0 +1,74 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the icecreamParlor function below.
+    static int[] icecreamParlor(int m, int[] arr) {
+        int[] result = {0,0};
+
+        for (int i=0;i<arr.length-1;i++){
+            int j;
+            result[0] = i+1;
+            int amtRem = m-arr[i];
+            for (j=i+1;j<arr.length;j++){
+                if(amtRem == arr[j]){
+                    result[1]=j+1;
+                }
+            }
+            if(result[1] != 0){
+                break;
+            }
+        }
+
+        return result;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int t = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int tItr = 0; tItr < t; tItr++) {
+            int m = scanner.nextInt();
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+            int n = scanner.nextInt();
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+            int[] arr = new int[n];
+
+            String[] arrItems = scanner.nextLine().split(" ");
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+            for (int i = 0; i < n; i++) {
+                int arrItem = Integer.parseInt(arrItems[i]);
+                arr[i] = arrItem;
+            }
+
+            int[] result = icecreamParlor(m, arr);
+
+            for (int i = 0; i < result.length; i++) {
+                bufferedWriter.write(String.valueOf(result[i]));
+
+                if (i != result.length - 1) {
+                    bufferedWriter.write(" ");
+                }
+            }
+
+            bufferedWriter.newLine();
+        }
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
